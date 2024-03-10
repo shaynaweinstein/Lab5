207803739e9bfe9c0e5e6583a8676e03175aaf6d
Shayna Weinstein <shayshayro1@gmail.com>
2024-03-09 22:45:16 -0600

    corrected code for program_2

--- program_2.c
+++ program_2.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-void findCombos(int score, int touchdowns, int fieldGoals, int safeties, int tdConversions, int fgTdConversions);
-void findComboshelp(int score, int touchdowns, int fieldGoals, int safeties, int tdConversions, int fgTdConversions);
+void findCombos(int score);
 int main(){
     int score;
 
@@ -18,40 +17,25 @@ while (1) {
     }
 
     printf("Possible scoring combinations for a score of %d:\n", score);
-    findCombos(score, 0, 0, 0, 0, 0);
+    findCombos(score);
     printf("\n");
 }
     return 0;
 }
-void findCombos(int score, int touchdowns, int fieldGoals, int safeties, int tdConversions, int fgTdConversions){
-    findComboshelp(score, touchdowns, fieldGoals, safeties, tdConversions, fgTdConversions);
+void findCombos(int score){
+for (int td2 = 0; td2 * 8 <= score; td2++){
+        for (int tdp = 0; tdp * 7 + td2 * 8 <= score; tdp++){
+            for (int td = 0; td * 6 + tdp * 7 + td2 * 8 <= score; td++){
+                for (int fg = 0; fg * 3 + td * 6 + tdp * 7 + td2 * 8 <= score; fg++){
+                    for (int sf = 0; sf * 2 + fg * 3 + td * 6 + tdp * 7 + td2 * 8 <= score; sf++){
+                        if (+ sf * 2 + fg * 3 + td * 6 + tdp * 7 + td2 * 8 == score){
+                            printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n", td2, tdp, td, fg, sf);
+            }
+                    }
+                }
+            }
 }
-void findComboshelp(int score, int touchdowns, int fieldGoals, int safeties, int tdConversions, int fgTdConversions){
-    if (score == 0) {
-        printf("%d TD(s) + %d FG(s) + %d Safety(s) + %d TD(s) + %d TD(s) + %d FG(s)\n",
-               touchdowns, fieldGoals, safeties, tdConversions, fgTdConversions, score);
-        return;
-    }
-
-    if (score >= 6) {
-        findComboshelp(score - 6, touchdowns + 1, fieldGoals, safeties, tdConversions, fgTdConversions);
-    }
-
-    if (score >= 3) {
-        findComboshelp(score - 3, touchdowns, fieldGoals + 1, safeties, tdConversions, fgTdConversions);
-    }
-
-    if (score >= 2) {
-        findComboshelp(score - 2, touchdowns, fieldGoals, safeties + 1, tdConversions, fgTdConversions);
-    }
-
-    if (score >= 8) {
-        findComboshelp(score - 8, touchdowns, fieldGoals, safeties, tdConversions + 1, fgTdConversions);
-    }
-
-    if (score >= 7) {
-        findComboshelp(score - 7, touchdowns, fieldGoals, safeties, tdConversions, fgTdConversions + 1);
-    }
+}
 }
 
 
